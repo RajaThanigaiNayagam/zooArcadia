7d36d84f6f107dfc1b7f2433be3323fa5d6fe39e
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2024-09-10 22:48:54 +0200

    templates error correction

--- src/Controller/Admin/ServiceController.php
+++ src/Controller/Admin/ServiceController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/service')]
+#[Route('/admin/zooservice')]
 class ServiceController extends AbstractController
 {
-    #[Route('/', name: 'app_admin_service_index', methods: ['GET'])]
+    #[Route('/', name: 'app_admin_zooservice_index', methods: ['GET'])]
     public function index(ServiceRepository $serviceRepository): Response
     {
         return $this->render('admin/zooservice/index.html.twig', [
@@ -22,7 +22,7 @@ class ServiceController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_service_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_admin_zooservice_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $service = new Service();
@@ -33,7 +33,7 @@ class ServiceController extends AbstractController
             $entityManager->persist($service);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_admin_service_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_zooservice_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('admin/zooservice/new.html.twig', [
@@ -50,7 +50,7 @@ class ServiceController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_admin_service_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_admin_zooservice_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Service $service, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ServiceType::class, $service);
@@ -59,7 +59,7 @@ class ServiceController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_admin_service_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_zooservice_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('admin/zooservice/edit.html.twig', [
@@ -68,7 +68,7 @@ class ServiceController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_service_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_admin_zooservice_delete', methods: ['POST'])]
     public function delete(Request $request, Service $service, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$service->getId(), $request->getPayload()->getString('_token'))) {
@@ -76,6 +76,6 @@ class ServiceController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_admin_service_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_admin_zooservice_index', [], Response::HTTP_SEE_OTHER);
     }
 }
